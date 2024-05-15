ff80965bb8a180b37af997814974ba3cdbdb6673
Jan Poláček <janik.polacek@gmail.com>
2024-05-15 22:45:31 +0200

    init: initialize project

---
+++ src/main/java/cz/polacek/sportcourt/SportCourtApplication.java
@@ -0,0 +1,13 @@
+package cz.polacek.sportcourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportCourtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportCourtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/polacek/sportcourt/SportCourtApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.polacek.sportcourt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportCourtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
